743ce3b551e95d588779396fd503debd81a935f9
ec-miller <ericmiller@umn.edu>
2019-06-23 17:15:40 -0500

    Updates

---
+++ precache-manifest.cf32d06c1f871f002e23e29b7d162d1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f053a98cb12405185903921510647409",
+    "url": "/spotify-album-lookup/index.html"
+  },
+  {
+    "revision": "312beacab9ad83ae8b72",
+    "url": "/spotify-album-lookup/static/css/main.6a3c525c.chunk.css"
+  },
+  {
+    "revision": "5c48657c9006d90d6d03",
+    "url": "/spotify-album-lookup/static/js/2.6932f60c.chunk.js"
+  },
+  {
+    "revision": "312beacab9ad83ae8b72",
+    "url": "/spotify-album-lookup/static/js/main.54294c40.chunk.js"
+  },
+  {
+    "revision": "ec2d9c9d070648411d12",
+    "url": "/spotify-album-lookup/static/js/runtime~main.c073bb44.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/spotify-album-lookup/precache-manifest.cf32d06c1f871f002e23e29b7d162d1f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/spotify-album-lookup/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
